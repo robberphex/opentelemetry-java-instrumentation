3ab5207808817588a22e1901d354876c663d463c
luyanbo <robert.lyb@alibaba-inc.com>
2024-03-24 01:07:33 +0800

    add jacoco to apache-httpclient-4.3/library,apache-httpclient-5.2/library

--- instrumentation/apache-httpclient/apache-httpclient-4.3/library/build.gradle.kts
+++ instrumentation/apache-httpclient/apache-httpclient-4.3/library/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("otel.library-instrumentation")
   id("otel.nullaway-conventions")
   id("otel.animalsniffer-conventions")
+  id("otel.jacoco-conventions")
 }
 
 dependencies {
@@ -10,4 +11,12 @@ dependencies {
   testImplementation(project(":instrumentation:apache-httpclient:apache-httpclient-4.3:testing"))
 
   latestDepTestLibrary("org.apache.httpcomponents:httpclient:4.+") // see apache-httpclient-5.0 module
+}
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
 }
--- instrumentation/apache-httpclient/apache-httpclient-5.2/library/build.gradle.kts
+++ instrumentation/apache-httpclient/apache-httpclient-5.2/library/build.gradle.kts
@@ -2,8 +2,17 @@ plugins {
   id("otel.library-instrumentation")
   id("otel.nullaway-conventions")
   id("otel.animalsniffer-conventions")
+  id("otel.jacoco-conventions")
 }
 
 dependencies {
   library("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+}
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
 }
